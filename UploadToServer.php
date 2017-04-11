d443c6be8c709856511958c9022f5d3e182cea0d
Ícaro Maia Santos de Castro <icaromscastro@gmail.com>
2017-04-11 12:14:18 -0300

    Service para upload de arquivos no servidor
    Realiza upload de arquivos do smartphone para uma pasta no servidor

---
+++ UploadToServer.php
@@ -0,0 +1,41 @@
+<?php
+  
+    
+$data = date("d-m-y");
+    
+$hora = date("H:i:s");
+    
+$arquivo = "logs/log_$data.txt";
+
+    
+$file_path = "uploads/";
+     
+    
+$file_path = $file_path . basename( $_FILES['uploadedfile']['name']);
+    
+
+if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $file_path)) {
+        
+	echo "success";
+        
+	$texto = "[$hora]> recebido com sucesso \n";
+    
+} else{
+        
+	echo "fail";
+        
+	$texto = "[$hora]> erro ao receber arquivo \n";
+    
+}
+
+    
+
+$manipular = fopen("$arquivo","a+b");
+    
+fwrite($manipular, "\n" . $texto);
+    
+fwrite($manipular, "File path: " . $file_path . "\n");
+    
+fclose($manipular);
+
+ ?>
